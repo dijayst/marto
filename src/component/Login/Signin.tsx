d8f6cbe6f61901b922cf7ebb03bcd5695ad33e71
esther olubiyi <olubiyisther@gmail.com>
2025-10-18 18:43:27 +0100

    test6

--- src/component/Login/Signin.tsx
+++ src/component/Login/Signin.tsx
@@ -30,7 +30,7 @@ export default function Signin() {
     formState: { errors },
   } = useForm<LoginFormInputs | SignupFormInputs>();
 
-  const onSubmit = async (data: any) => {
+  const onSubmit789 = async (data: any) => {
     const { email, password } = data;
     const { setIsLoggedIn, setCurrentUser } = useAuthStoree.getState();
 
@@ -44,6 +44,67 @@ export default function Signin() {
     try {
       const endpoint = isLogin ? "/api/auth/login" : "/api/auth/signup";
       const res = await fetch(`${apiurl}/endpoint`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const result = await res.json();
+      // if (!res.ok) throw new Error(result.error || "User already exists");
+
+      if (!res.ok) {
+        if (isLogin && result.error?.toLowerCase().includes("not found")) {
+          throw new Error("User does not exist");
+        }
+        throw new Error(
+          result.error ||
+            (isLogin ? "User does not exist" : "User already exists")
+        );
+      }
+
+      // ✅ Save user state in Zustand
+      setIsLoggedIn(true);
+      setCurrentUser({ email: result.user.email, role: "user" });
+
+      localStorage.setItem("userEmail", result.user.email);
+      alert(result.message);
+
+      // ✅ Redirect back to Get Quotes if needed
+      const redirectPath = localStorage.getItem("redirectAfterLogin");
+      if (redirectPath) {
+        localStorage.removeItem("redirectAfterLogin");
+        router.push(redirectPath);
+      } else {
+        router.push("/");
+      }
+    } catch (err: any) {
+      //catch (err: any) {      alert(err.message);      setServerError(err.message);}
+
+      alert(err.message);
+      if (err instanceof Error) {
+        setServerError(err.message);
+      } else {
+        setServerError("An unexpected error occurred");
+      }
+    }
+  };
+
+  const onSubmit = async (data: any) => {
+    const { email, password } = data;
+    const { setIsLoggedIn, setCurrentUser } = useAuthStoree.getState();
+
+    if (email === "admin@site.com" && password === "Admin$123") {
+      login("admin");
+      router.push("/admin");
+      alert("Admin login successful ✅");
+      return;
+    }
+
+    const apiurl = process.env.API_URL || "http://localhost:3000";
+    try {
+      const endpoint = isLogin ? "/api/auth/login" : "/api/auth/signup";
+      // const res = await fetch(endpoint, {
+      const res = await fetch(`${apiurl}${endpoint}`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
